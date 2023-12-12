5a0ae526ef52c373105ca814ebfd4e64b166d410
Mihail Kostov <mishoka.kostov@gmail.com>
2023-12-12 14:26:33 -0500

    As operator, Abstract classes, Abstract methods

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -364,38 +364,73 @@
 //var ingredient = new Ingredient(1);
 //var cheddar = new Cheddar(2, 12);
 //Console.WriteLine(cheddar);
-Ingredient ingredient = new Cheddar(2, 12);
+//Ingredient ingredient = new Cheddar(2, 12);
 
-Ingredient randomIngredient = GenerateRandomIngredient();
-Console.WriteLine("Random ingredient is " + randomIngredient);
+//Ingredient randomIngredient = GenerateRandomIngredient();
+//Console.WriteLine("Random ingredient is " + randomIngredient);
 
-Console.WriteLine("is object? " + (ingredient is object));
-Console.WriteLine("is ingredient? " + (ingredient is Ingredient));
-Console.WriteLine("is cheaddar? " + (ingredient is Cheddar));
-Console.WriteLine("is mozzarella? " + (ingredient is Mozzarella));
-Console.WriteLine("is tomato sause? " + (ingredient is TomatoSause));
+//Console.WriteLine("is object? " + (ingredient is object));
+//Console.WriteLine("is ingredient? " + (ingredient is Ingredient));
+//Console.WriteLine("is cheaddar? " + (ingredient is Cheddar));
+//Console.WriteLine("is mozzarella? " + (ingredient is Mozzarella));
+//Console.WriteLine("is tomato sause? " + (ingredient is TomatoSause));
 
 
-if (randomIngredient is Cheddar cheddar)
-{
-	Console.WriteLine("cheddar object: " + cheddar);
-}
+//if (randomIngredient is Cheddar cheddar)
+//{
+//	Console.WriteLine("cheddar object: " + cheddar);
+//}
 
-var pizza = new Pizza();
-Console.WriteLine(pizza.ingredient);
+//var pizza = new Pizza();
+//Console.WriteLine(pizza.ingredient);
 
-Ingredient nullIngredient = null;
+//Ingredient nullIngredient = null;
 
 //if(nullIngredient != null)
 //{
 
 //}
 
-if (nullIngredient is not null)
-{
-	Console.WriteLine(nullIngredient.Name);
-}
+//if (nullIngredient is not null)
+//{
+//	Console.WriteLine(nullIngredient.Name);
+//}
 
+//Ingredient ingredient = GenerateRandomIngredient();
+//Console.WriteLine("Ingredient is " + ingredient);
+
+//Cheddar cheddar = ingredient as Cheddar;
+//Cheddar cheddar = ingredient as Cheddar;
+
+//if (cheddar is not null)
+//{
+//	Console.WriteLine(cheddar.Name);
+//}
+//else
+//{
+//	Console.WriteLine("Conversion failed");
+//}
+
+using System.Threading.Channels;
+
+//Ingredient ingredient = new Ingredient(1);
+
+var cheddar = new Cheddar(2, 12);
+var tomatoSauce = new TomatoSause(1);
+cheddar.Prepare();
+tomatoSauce.Prepare();
+
+var ingredients = new List<Ingredient>
+{
+	new Cheddar(2,10),
+	new Mozzarella(2),
+	new TomatoSause(2),
+};
+
+foreach (var ingredient in ingredients)
+{
+	ingredient.Prepare();
+}
 
 Console.ReadKey();
 
@@ -421,7 +456,7 @@ public class Pizza
 	public override string ToString() => $"This is a pizza with {string.Join(", ", _ingredients)}";
 }
 
-public class Ingredient
+public abstract class Ingredient
 {
 	public Ingredient(int priceIfExtraTopping)
 	{
@@ -431,6 +466,8 @@ public class Ingredient
 	public int PriceIfExtraTopping { get; }
 	public override string ToString() => Name;
 	public virtual string Name { get; } = "Some ingredient";
+
+	public abstract void Prepare();
 
 	public int PublicField;
 
@@ -444,7 +481,7 @@ public class Ingredient
 		"This method is PRIVATE in the Ingredient class.";
 }
 
-public class Cheese : Ingredient
+public abstract class Cheese : Ingredient
 {
 	public Cheese(int priceIfExtraTopping) : base(priceIfExtraTopping)
 	{
@@ -456,7 +493,7 @@ public class ItalianFoodItem
 }
 
 
-public class Cheddar : Ingredient
+public class Cheddar : Cheese
 {
 	public Cheddar(int priceIfExtraTopping, int agedForMonths) : base(priceIfExtraTopping)
 	{
@@ -467,6 +504,10 @@ public class Cheddar : Ingredient
 		$"{base.Name}, more specifically, " +
 		$"a Cheddar cheese aged for {AgedForMonths} months";
 	public int AgedForMonths { get; }
+
+	public override void Prepare() =>
+		Console.WriteLine("Grate and sprikle over pizza.");
+
 
 	public void UseMethodsFromBaseClass()
 	{
@@ -482,8 +523,12 @@ public class TomatoSause : Ingredient
 	{
 	}
 
-	public string Name => "Tomato sause";
+	public override string Name => "Tomato sause";
 	public int TomatosIn100Grams { get; }
+
+	public override void Prepare() =>
+		Console.WriteLine("Cock tomatoes with basil, garlic and salt." +
+			"Spread on pizza.");
 }
 
 public class Mozzarella : Cheese
@@ -494,6 +539,10 @@ public class Mozzarella : Cheese
 
 	public override string Name => "Mozzarella";
 	public bool IsLight { get; }
+
+	public override void Prepare() =>
+
+		Console.WriteLine("Slice thinly and place on top of the pizza.");
 }
 
 //var numbers = new List<int> { 1, 4, 6, -1, 12, 44, -8, -19 };
