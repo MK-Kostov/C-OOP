c198908a533b03f8ccd3f3ef3c71860a444a270b
Mihail Kostov <mishoka.kostov@gmail.com>
2023-12-15 10:06:41 -0500

    Assignment – Cookies Cookbook – Designing data types

---
+++ CookieCookbook/Recipes/Ingredients/Chocolate.cs
@@ -0,0 +1,10 @@
+﻿namespace CookieCookbook.Recipes.Ingredients
+{
+    public class Chocolate : Ingredient
+    {
+        public override int Id => 4;
+        public override string Name => "Chocolate";
+        public override string PreparationInstructions =>
+            $"Melt on water bath. {base.PreparationInstructions}";
+    }
+}
