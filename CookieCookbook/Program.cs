d135806c74332d58d2b04103f4f8a313a2d4d539
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-25 06:50:22 -0500

    Assignment – Refactoring to LINQ – Nested loop and code readability

--- CookieCookbook/Program.cs
+++ CookieCookbook/Program.cs
@@ -209,16 +209,9 @@ public class RecipesRepository : IRecipesRepository
 
 	private Recipe RecipeFromString(string recipeFromFile)
 	{
-		var textualIds = recipeFromFile.Split(Separator);
-		var ingredients = new List<Ingredient>();
-
-		foreach (var textualId in textualIds)
-		{
-			var id = int.Parse(textualId);
-			var ingredient = _ingredientsRegister.GetById(id);
-			ingredients.Add(ingredient);
-
-		}
+		var ingredients = recipeFromFile.Split(Separator)
+			.Select(int.Parse)
+			.Select(_ingredientsRegister.GetById);
 
 		return new Recipe(ingredients);
 
@@ -226,17 +219,13 @@ public class RecipesRepository : IRecipesRepository
 
 	public void Write(string filePath, List<Recipe> allRecipes)
 	{
-		var recipesAsStrings = new List<string>();
-		foreach (var recipe in allRecipes)
+		var recipesAsStrings = allRecipes.Select(recipe =>
 		{
-			var allIds = new List<int>();
-			foreach (var ingredient in recipe.Ingredients)
-			{
-				allIds.Add(ingredient.Id);
-			}
-			recipesAsStrings.Add(string.Join(Separator, allIds));
-		}
-		_stringsRepository.Write(filePath, recipesAsStrings);
+			var allIds = recipe.Ingredients.Select(ingredient => ingredient.Id);
+			return string.Join(Separator, allIds);
+		});
+
+		_stringsRepository.Write(filePath, recipesAsStrings.ToList());
 	}
 }
 
